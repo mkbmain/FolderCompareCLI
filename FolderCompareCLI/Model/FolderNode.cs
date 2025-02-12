67e2dc2c42b6ba950e00a8c245b0d03522b300f5
mkbmain <michael.bourke@live.co.uk>
2025-02-12 01:14:49 +0000

    ww

--- FolderCompareCLI/Model/FolderNode.cs
+++ FolderCompareCLI/Model/FolderNode.cs
@@ -1,4 +1,4 @@
 namespace FolderCompareCLI.Model;
 
 internal sealed record FolderNode(string Name, string FullPath, IList<FolderNode> SubDirectories, IList<FileNode> Files) :
-    FileNode(Name, FullPath, Files.Sum(w => w.Size) + Files.Sum(q => q.Size));
+    FileNode(Name, FullPath, Files.Sum(w => w.Size) + SubDirectories.Select(w=> w.Files.Sum(q=> q.Size)).Sum());
--- FolderCompareCLI/Program.cs
+++ FolderCompareCLI/Program.cs
@@ -28,23 +28,27 @@ public class Program
         var sourcePath = args[0].TrimEnd(FileAndIoUtils.DirectorySeparator);
         var destinationPath = args[1].TrimEnd(FileAndIoUtils.DirectorySeparator);
         var calcHash = args.Length > 2 && args[2] == "-hash";
-
+        var sourcePathWithDirSeparator = $"{sourcePath}{FileAndIoUtils.DirectorySeparator}";
+        var destinationPathWithDirSeparator = $"{destinationPath}{FileAndIoUtils.DirectorySeparator}";
         if (!StrIsPath(sourcePath) || !StrIsPath(destinationPath)) return;
-        if (sourcePath.Contains(destinationPath) || destinationPath.Contains(sourcePath))
+        if (sourcePathWithDirSeparator.Contains(destinationPathWithDirSeparator) ||
+            destinationPathWithDirSeparator.Contains(sourcePathWithDirSeparator))
         {
             Console.WriteLine("Recursive check.. Directories are the same or one is child\\subdirectory of the other");
             return;
         }
 
         Console.Clear();
-        
+
         Exception exception = null;
         Task.Run(() =>
         {
             try
             {
-                var (sourceFolderNodes, destinationFolderNodes) = BuildNodeUtils.BuildFolderPaths(sourcePath, destinationPath);
-                _differenceNodeViews = BuildNodeUtils.CalculateDifferences(sourceFolderNodes, destinationFolderNodes, calcHash)
+                var (sourceFolderNodes, destinationFolderNodes) =
+                    BuildNodeUtils.BuildFolderPaths(sourcePath, destinationPath);
+                _differenceNodeViews = BuildNodeUtils
+                    .CalculateDifferences(sourceFolderNodes, destinationFolderNodes, calcHash)
                     .Select(w => new DifferenceNodeView(w, sourcePath, destinationPath))
                     .ToDictionary(q => q.Id);
             }
@@ -81,7 +85,7 @@ public class Program
         int pageNumber = 0;
         while (true)
         {
-            Console.SetCursorPosition(0,0);
+            Console.SetCursorPosition(0, 0);
             Console.Clear();
             if (_differenceNodeViews.Any() == false)
             {
@@ -95,7 +99,9 @@ public class Program
             allowedNodes = allowedNodes < 1 ? 1 : allowedNodes;
             var differenceNodes = _differenceNodeViews.Chunk(allowedNodes).ToArray();
             pageNumber = pageNumber < differenceNodes.Length ? pageNumber : differenceNodes.Length - 1;
-            var page = pageNumber > differenceNodes.Length ? differenceNodes.Last() : differenceNodes.Skip(pageNumber).First();
+            var page = pageNumber > differenceNodes.Length
+                ? differenceNodes.Last()
+                : differenceNodes.Skip(pageNumber).First();
             Console.WriteLine("Option -- details");
             for (var index = 0; index < page.Length; index++)
             {
